75f50e06ac647a63f61a3c1ef293665fa6829cb1
Jon Gorman <contact@jongorman.top>
2019-12-12 10:47:46 -0600

    added build folder

---
+++ client/build/precache-manifest.b3e72f5e3f9e8062b2640bfdb0be9b91.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98cd365e31b93ebe978cd2a300dca95a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71f8c76c9c8001374e27",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "600a225a6818988b6b2c",
+    "url": "/static/css/main.993b79dc.chunk.css"
+  },
+  {
+    "revision": "71f8c76c9c8001374e27",
+    "url": "/static/js/2.13235393.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.13235393.chunk.js.LICENSE"
+  },
+  {
+    "revision": "600a225a6818988b6b2c",
+    "url": "/static/js/main.709d715a.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "596a61fe32c5c4924167a957858b4e14",
+    "url": "/static/media/prime_now_logo_Amazon.596a61fe.jpg"
+  }
+]);
